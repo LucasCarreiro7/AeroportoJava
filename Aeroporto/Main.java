b04bace8a932bb837e50a0aab7c296e07c1332a9
Lucas Carreiro <162310751+LucasCarreiro7@users.noreply.github.com>
2025-02-27 14:05:40 -0300

    Update Main.java

--- Aeroporto/Main.java
+++ Aeroporto/Main.java
@@ -2,7 +2,7 @@ package Aeroporto;
 
 public class Main {
     public static void main(String[] args) {
-    Piloto piloto1 = new Piloto("Samuel Victor", 1, 69, "Angolano", 45, 300000);
+    Piloto piloto1 = new Piloto("John Doe", 1, 70, "Inglês", 45, 300000);
     // Piloto piloto2 = new Piloto("Rafael Lopes", 2, 41, "Brasileiro", 14, 140000);
 
     Aeronave aeronave1 = new Aeronave("Boeing", "Boeing 737", "LATAM", 215);
